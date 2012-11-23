e54746830b451a050c19daa85cc04f6a663f3761
Per Cederberg <per@percederberg.net>
2012-11-23 15:38:50 +0000

    Updated install script.

--- install.sh
+++ install.sh
@@ -16,10 +16,22 @@ fail() {
 }
 
 # Check for root user
-[ `whoami` != 'root' ] || fail "only root is allowed to install admin-scripts"
+[ `whoami` == 'root' ] || fail "only root is allowed to install admin-scripts"
 
 # Install required packages
+echo "Installing dependencies..."
 apt-get -qq install rsync lzma libgeo-ipfree-perl libjson-xs-perl
 
-# Install binary files
+# Install script files
+echo "Installing admin scripts..."
+install --mode=0744 bin/admin-backup-files /usr/local/bin/
+install bin/admin-backup-mysql /usr/local/bin/
+install bin/admin-backup-sync /usr/local/bin/
+install bin/admin-freemem /usr/local/bin/
 install bin/admin-status /usr/local/bin/
+install --mode=0744 bin/admin-uptodate /usr/local/bin/
+install bin/admin-utf8 /usr/local/bin/
+
+# Finished
+echo "...done"
+echo "Note: Config files must be installed manually."
