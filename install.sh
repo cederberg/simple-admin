80fedbbfd67c23453afc47c04e75805ae71849c8
Per Cederberg <per@percederberg.net>
2012-11-28 15:32:49 +0000

    Updated admin-www-logrotate to use config file.

--- install.sh
+++ install.sh
@@ -32,6 +32,7 @@ install --mode=0744 bin/admin-restart /usr/local/bin/
 install bin/admin-status /usr/local/bin/
 install --mode=0744 bin/admin-uptodate /usr/local/bin/
 install bin/admin-utf8 /usr/local/bin/
+install --mode=0744 bin/admin-www-logrotate /usr/local/bin/
 install bin/admin-www-stats /usr/local/bin/
 install bin/admin-www-webalizer /usr/local/bin/
 
