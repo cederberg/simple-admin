0df6b3f35b7e628fd91b59156c9cb1855e27a5eb
Per Cederberg <per@percederberg.net>
2013-03-21 16:48:21 +0000

    Added installation of simple-fileinfo.

--- install.sh
+++ install.sh
@@ -28,6 +28,7 @@ install --mode=0744 bin/admin-backup-files /usr/local/bin/
 install bin/admin-backup-mysql /usr/local/bin/
 install bin/admin-backup-status /usr/local/bin/
 install bin/admin-backup-sync /usr/local/bin/
+install bin/simple-fileinfo /usr/local/bin/
 install bin/admin-freemem /usr/local/bin/
 install --mode=0744 bin/admin-restart /usr/local/bin/
 install bin/admin-status /usr/local/bin/
